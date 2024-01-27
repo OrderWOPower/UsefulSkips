ef14cd250a8b33c643ed4d6febd0fb96330d89a2
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2024-01-28 01:56:44 +0800

    Refactored the code.

---
+++ UsefulSkipsGameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace UsefulSkips
+{
+    [HarmonyPatch(typeof(GameStateManager), "CleanAndPushState")]
+    public class UsefulSkipsGameStateManager
+    {
+        public static void Prefix(GameState gameState)
+        {
+            if (gameState is VideoPlaybackState videoPlaybackState && videoPlaybackState.VideoPath.Contains("campaign_intro") && UsefulSkipsSettings.Instance.ShouldSkipCampaignIntro)
+            {
+                AccessTools.Property(typeof(VideoPlaybackState), "AudioPath").SetValue(videoPlaybackState, null);
+            }
+        }
+
+        public static void Postfix(GameState gameState)
+        {
+            if (gameState is VideoPlaybackState videoPlaybackState && videoPlaybackState.VideoPath.Contains("campaign_intro") && UsefulSkipsSettings.Instance.ShouldSkipCampaignIntro)
+            {
+                videoPlaybackState.OnVideoFinished();
+            }
+        }
+    }
+}
--- UsefulSkipsSubModule.cs
+++ UsefulSkipsSubModule.cs
@@ -1,7 +1,4 @@
 ﻿using HarmonyLib;
-using SandBox;
-using StoryMode;
-using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 using Module = TaleWorlds.MountAndBlade.Module;
 
@@ -10,13 +7,7 @@ namespace UsefulSkips
     // This mod adds options to skip the splash screen, campaign intro, character creation and tutorial.
     public class UsefulSkipsSubModule : MBSubModuleBase
     {
-        private Harmony _harmony;
-
-        protected override void OnSubModuleLoad()
-        {
-            _harmony = new Harmony("mod.bannerlord.usefulskips");
-            _harmony.PatchAll();
-        }
+        protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.usefulskips").PatchAll();
 
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
@@ -24,32 +15,6 @@ namespace UsefulSkips
             {
                 // Skip the splash screen.
                 AccessTools.Field(typeof(Module), "_splashScreenPlayed").SetValue(Module.CurrentModule, true);
-            }
-        }
-
-        public override void OnNewGameCreated(Game game, object initializerObject)
-        {
-            if (UsefulSkipsSettings.Instance.ShouldSkipCampaignIntro)
-            {
-                _harmony.Patch(AccessTools.Method(typeof(SandBoxGameManager), "OnLoadFinished"), transpiler: new HarmonyMethod(AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler")));
-                _harmony.Patch(AccessTools.Method(typeof(StoryModeGameManager), "OnLoadFinished"), transpiler: new HarmonyMethod(AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler")));
-
-                // Check whether BKCE is loaded.
-                if (AccessTools.TypeByName("BKCEGameManager") != null)
-                {
-                    _harmony.Patch(AccessTools.Method(AccessTools.TypeByName("BKCEGameManager"), "OnLoadFinished"), transpiler: new HarmonyMethod(AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler")));
-                }
-            }
-        }
-
-        public override void OnGameEnd(Game game)
-        {
-            _harmony.Unpatch(AccessTools.Method(typeof(SandBoxGameManager), "OnLoadFinished"), AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler"));
-            _harmony.Unpatch(AccessTools.Method(typeof(StoryModeGameManager), "OnLoadFinished"), AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler"));
-
-            if (AccessTools.TypeByName("BKCEGameManager") != null)
-            {
-                _harmony.Unpatch(AccessTools.Method(AccessTools.TypeByName("BKCEGameManager"), "OnLoadFinished"), AccessTools.Method(typeof(UsefulSkipsGameManager), "Transpiler"));
             }
         }
     }

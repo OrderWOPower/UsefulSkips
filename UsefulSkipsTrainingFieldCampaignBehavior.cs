d5e67b975d97e7244017751962f51461dc051409
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2025-10-13 16:28:46 +0800

    Separated nested classes

---
+++ UsefulSkipsTrainingFieldCampaignBehavior.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using StoryMode;
+using StoryMode.GameComponents.CampaignBehaviors;
+using StoryMode.StoryModePhases;
+using TaleWorlds.CampaignSystem.GameState;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+
+namespace UsefulSkips
+{
+    [HarmonyPatch(typeof(TrainingFieldCampaignBehavior), "OnCharacterCreationIsOver")]
+    public class UsefulSkipsTrainingFieldCampaignBehavior
+    {
+        private static void Prefix(ref bool ___SkipTutorialMission)
+        {
+            if (UsefulSkipsSettings.Instance.ShouldSkipTutorial)
+            {
+                // Skip the tutorial.
+                ___SkipTutorialMission = true;
+
+                MobileParty.MainParty.Position = Settlement.Find("tutorial_training_field").Position;
+                ((MapState)GameStateManager.Current.ActiveState)?.Handler.TeleportCameraToMainParty();
+                TutorialPhase.Instance.PlayerTalkedWithBrotherForTheFirstTime();
+                StoryModeManager.Current.MainStoryLine.CompleteTutorialPhase(false);
+            }
+        }
+    }
+}
---
+++ UsefulSkipsTutorialPhaseCampaignBehavior.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using Helpers;
+using StoryMode.GameComponents.CampaignBehaviors;
+using StoryMode.StoryModeObjects;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Localization;
+
+namespace UsefulSkips
+{
+    [HarmonyPatch(typeof(TutorialPhaseCampaignBehavior), "OnStoryModeTutorialEnded")]
+    public class UsefulSkipsTutorialPhaseCampaignBehavior
+    {
+        private static bool Prefix()
+        {
+            if (UsefulSkipsSettings.Instance.ShouldSkipTutorial)
+            {
+                TextObject textObject = FactionHelper.GenerateClanNameforPlayer();
+
+                // Ensure that the player's clan name matches the selected culture.
+                Clan.PlayerClan.ChangeClanName(textObject, textObject);
+                DisableHeroAction.Apply(StoryModeHeroes.ElderBrother);
+                StoryModeHeroes.ElderBrother.Clan = null;
+                PartyBase.MainParty.ItemRoster.Clear();
+                PartyBase.MainParty.ItemRoster.AddToCounts(DefaultItems.Grain, 2);
+                Hero.MainHero.Heal(Hero.MainHero.MaxHitPoints, false);
+                Hero.MainHero.Gold = 1000;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
